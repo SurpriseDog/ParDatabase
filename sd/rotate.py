49288a99f14f56d21b1dd00034b677629fd90d73
benruss64@gmail.com <49625187+SurpriseDog@users.noreply.github.com>
2023-02-21 10:38:06 -0600

    rotate now stops at file gaps

--- sd/rotate.py
+++ sd/rotate.py
@@ -1,32 +1,47 @@
 #!/usr/bin/python3
+# Usage: rotate.py <filename> limit prefix
+
 import os
 import sys
 
 def rotate(path, limit=3, prefix='.', move=True, verbose=False):
-    '''Given a file path, move files through a progression until limit is reached and delete oldest file,.''
-    limit = Max number of files before deleting final one
-    prefix = add a prefix before each number
+    '''Given a file path, Rotate files in a sequence 1, 2, 3... up to limit,
+    and delete last file if and only if the sequence is full with no gaps
+
+    limit = Max number of backup files in sequence (not including original)
+    prefix = Add a prefix before each number
     move = Actually move the files instead of just listing them
 
-    Returns list of files in sequence
+    Returns sequence of files (whether or not there are any gaps)
 
     '''
     files = [path]
     path = os.path.splitext(path)
     files += [path[0] + prefix + str(num) + path[1] for num in range(1, limit+1)]
-    dest = files.pop(-1)
+    print(files)
     if move:
-        if os.path.exists(dest):
+
+        # Go thru file list looking for the first missing file
+        gap = 0         # Position of first missing file
+        for gap, name in enumerate(files):
+            if not os.path.exists(name):
+                break
+        else:
+            dest = files[-1]
             if verbose:
                 print("Removing", dest)
-            os.remove(dest)
-        for src in reversed(files):
-            if os.path.exists(src):
-                if verbose:
-                    print("Moving", src, dest)
-                os.rename(src, dest)
+                os.remove(dest)
+
+        # Go thru file list backwards, moving each one
+        dest = files[gap]
+        for src in reversed(files[:gap]):
+            if verbose:
+                print("Moving", src, dest)
+            os.rename(src, dest)
             dest = src
+
     return files
+
 
 
 def main():
@@ -46,7 +61,7 @@ def main():
     else:
         prefix = '.'
 
-    rotate(filename, limit, prefix)
+    rotate(filename, limit, prefix, verbose=True)
     return True
 
 
